6905ea50e4a8663ca39d548770344288fafa3bd6
Bello Osagie <osagienoah555@gmail.com>
2020-02-12 02:16:21 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const send1 = document.querySelector('.span1');
+const send2 = document.querySelector('.span2');
+const send3 = document.querySelector('.span3');
+
+const eventHandler = () => {
+  send1.style.backgroundColor = 'blue';
+  send1.style.borderRadius = '4px';
+  send1.style.padding = '5px';
+  send1.style.width = '160px';
+  send1.style.display = 'block';
+  send1.style.left = '80px';
+  
+  send2.style.backgroundColor = 'blue';
+  send2.style.borderRadius = '4px';
+  send2.style.padding = '5px';
+  send2.style.left = '80px';
+  send2.style.display = 'block';
+  send2.style.width = '160px';
+
+  send3.style.backgroundColor = 'blue';
+  send3.style.borderRadius = '4px';
+  send3.style.padding = '5px';
+  send3.style.left = '80px';
+  send3.style.width = '160px';
+  send3.style.display = 'block';
+};
+
+send.onclick = eventHandler;
