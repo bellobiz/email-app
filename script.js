7b61ef0c7e67d7db61beb5d9393ee4d2cdb6c9cc
Bello Osagie <49061129+belloosagienoah@users.noreply.github.com>
2020-02-15 16:57:00 +0100

    Format File

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ const button1 = document.querySelector(".button1");
 const button2 = document.querySelector(".button2");
 const button3 = document.querySelector(".button3");
 
-const eventHandler = () => {
+const eventHandler1 = () => {
   button1.style.visibility = "visible";
   button2.style.visibility = "visible";
   button3.style.visibility = "visible";
+};
+
+const eventHandler2 = () => {
+  button1.style.visibility = "";
+  button2.style.visibility = "";
+  button3.style.visibility = "";
 };
